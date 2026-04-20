fb46cd9db4db3a4ef96498e4a3600dfa1f8f1644
drashti-thekdi <85118452+drashti-thekdi@users.noreply.github.com>
2026-04-20 15:59:31 +0530

    Refactor App component to use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar'
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Footer from './components/Footer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer />
-    </>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
